1b58847e55025da150a3f33fcd495fdc4accf599
Nicola Beghin <nicolabeghin@gmail.com>
2024-02-27 18:44:10 +0100

    fix import SAML metadata for CIE

--- src/http.js
+++ src/http.js
@@ -43,7 +43,7 @@ exports.httpGrabKeycloaktoken = httpGrabKeycloaktoken
 
 exports.httpCallKeycloakImportConfig = function (idPsMetadataUrl) {
     return httpGrabKeycloaktoken().then(token => {
-        let data = JSON.stringify({"providerId": "spid", "fromUrl": idPsMetadataUrl});
+        let data = JSON.stringify({"providerId": "cieid", "fromUrl": idPsMetadataUrl});
         let axiosConfig = {
             httpsAgent : agent,
             method: 'post',
